a173697fda6f764d1cc7f65d744664b0f943bbaa
lasertown <jasonju@gmail.com>
2021-03-03 16:16:05 -0800

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+# Create Resource Group
 RgName="test1"
 Location="westus2"
 az group create --name $RgName --location $Location
+
+# Create Public IP
 PipName="myPublicIP"
 az network public-ip create --name $PipName --resource-group $RgName --location $Location
+
+# Create VNET
 VnetName="myVnet"
 VnetPrefix="10.0.0.0/16"
 VnetSubnetName="mySubnet"
 VnetSubnetPrefix="10.0.0.0/24"
-
 az network vnet create \
         --name $VnetName \
         --resource-group $RgName \
@@ -17,16 +21,6 @@ az network vnet create \
         --address-prefix $VnetPrefix \
         --subnet-name $VnetSubnetName \
         --subnet-prefix $VnetSubnetPrefix
-
-NicName="MyNic1"
-az network nic create \
-        --name $NicName \
-        --resource-group $RgName \
-        --location $Location \
-        --subnet $VnetSubnetName \
-        --private-ip-address 10.0.0.4 \
-        --vnet-name $VnetName \
-        --public-ip-address $PipName
 
 # Create NSG
 NSGname="grantnsg"
@@ -40,23 +34,25 @@ az network nsg rule create \
         --access Allow \
         --protocol Tcp \
         --description "Accept SSH connections from my public IP"
-        
+
+# Create NIC
+NicName="MyNic1"
+az network nic create \
+        --name $NicName \
+        --resource-group $RgName \
+        --location $Location \
+        --subnet $VnetSubnetName \
+        --network-security-group $NSGname \
+        --private-ip-address 10.0.0.4 \
+        --vnet-name $VnetName \
+        --public-ip-address $PipName
+       
 # Create a VM and attach the NIC.
 VmName="myVm"
 VmSize="Standard_A4_v2"
-OsImage="SUSE:sles-15-sp2:gen2:2021.02.05"
+OsImage="SUSE:sles-15-sp2:gen2:latest"
 Username="azadmin"
 SshKeyValue="</path/to/local/file>"
-az network nsg rule create \
-        --name "Allow SSH" \
-        --resource-group $RgName \
-        --nsg-name grantnsg \
-        --priority 100 \
-        --source-address-prefixes $(curl -s ifconfig.me)/32 \
-        --destination-port-ranges 22 \
-        --access Allow \
-        --protocol Tcp \
-        --description "Accept SSH connections from my public IP"
 az vm create \
         --name $VmName \
         --resource-group $RgName \
@@ -64,6 +60,5 @@ az vm create \
         --location $Location \
         --size $VmSize \
         --nics $NicName \
-        --nsg $NSGname \
         --admin-username $Username \
         --ssh-key-value $SshKeyValue \
